79508ce352af6cfbc43a924c5ec14c0f796d72cb
Leon Chen <chenlinfeng1024@gmail.com>
2020-02-15 14:55:12 +0800

    complete 1.3.49

---
+++ alg4/src/edu/princeton/cs/exercise/chapter1_3/E01_03_49.java
@@ -0,0 +1,19 @@
+package edu.princeton.cs.exercise.chapter1_3;
+
+/**
+ * 1.3.49 Queue with three stacks. Implement a queue with three stacks so that each queue operation
+ * takes a constant (worst-case) number of stack operations. Warning : high degree of difficulty.
+ *
+ * <p>使用三个栈的 Queue . 实现一个队列使用三个栈来使得每个 queue 操作消耗一个常量级(最差情况)的栈操作.警告:高难度.
+ *
+ * @author LeonChen
+ * @since 2/14/20
+ */
+class E01_03_49 {
+
+  public static void main(String[] args) {
+
+    // 貌似还没有使用 三个栈来解决的方式. 具体可以参考这个讨论
+    // https://stackoverflow.com/questions/5538192/how-to-implement-a-queue-with-three-stacks/
+  }
+}
