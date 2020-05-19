34ef0ded989ece767f4ae830e9349fcf1687af31
Leon Chen <chenlinfeng1024@gmail.com>
2020-05-19 08:19:45 +0800

    update 2.1.1 & 2

---
+++ alg4/src/edu/princeton/cs/exercise/chapter2_1/E02_01_02.java
@@ -0,0 +1,19 @@
+package edu.princeton.cs.exercise.chapter2_1;
+
+/**
+ * 2.1.2 What is the maximum number of exchanges involving any particular element during selection
+ * sort? What is the average number of exchanges involving an element?
+ *
+ * <p>选择排序中任意元素的交换次数的最大值是多少? 一个元素的平均交换次数是多少?
+ *
+ * @author LeonChen
+ * @since 5/13/20
+ */
+class E02_01_02 {
+
+  /**
+   * 最大值是N-1,当第一个数正好要排到最后一个数的时候,或者相反.<br>
+   * 平均交换次数是 1 .
+   */
+  public static void main(String[] args) {}
+}
