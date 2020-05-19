a26869cb2278a768ad020fb5972f28ed93fce4fd
Leon Chen <chenlinfeng1024@gmail.com>
2020-05-20 07:49:54 +0800

    update 2.1.3&4

---
+++ alg4/src/edu/princeton/cs/exercise/chapter2_1/E02_01_03.java
@@ -0,0 +1,17 @@
+package edu.princeton.cs.exercise.chapter2_1;
+
+/**
+ * 2.1.3 Give an example of an array of N items that maximizes the number of times the test a[j] <
+ * a[min] fails (and, therefore, min gets updated) during the operation of selection sort (Algorithm
+ * 2.1).
+ *
+ * <p>给出一个有 N个项的例子,它在选择排序 (Algorithm 2.1) 中判断 a[j]<a[min] 失败的次数是最多的(并且,尽管如此,min 得到了更新)
+ *
+ * @author LeonChen
+ * @since 5/13/20
+ */
+class E02_01_03 {
+
+  /** 只要是一个倒序的数组即可 如 f e d c b a */
+  public static void main(String[] args) {}
+}
