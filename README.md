a1e4b604fa799a925b38eaafebf33a5b85ad90e6
Leon Chen <chenlinfeng1024@gmail.com>
2021-02-01 10:35:54 +0800

    add data link

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # algorithm-4th-exercise
 algorithm 4th exercise 
+
+
+
+
+
+输入数据下载地址
+
+https://algs4.cs.princeton.edu/code/algs4-data.zip
+
+下载太慢或者无法下载的可以通过我这里的下载
+
+链接: https://pan.baidu.com/s/1kP5BcS43GZvBM95yw_dj4A 提取码: 7zpc 
--- alg4/src/edu/princeton/cs/algs4/FrequencyCounter.java
+++ alg4/src/edu/princeton/cs/algs4/FrequencyCounter.java
@@ -27,21 +27,22 @@
 package edu.princeton.cs.algs4;
 
 /**
- *  The {@code FrequencyCounter} class provides a client for 
- *  reading in a sequence of words and printing a word (exceeding
- *  a given length) that occurs most frequently. It is useful as
- *  a test client for various symbol table implementations.
- *  <p>
- *  For additional documentation, see <a href="https://algs4.cs.princeton.edu/31elementary">Section 3.1</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * The {@code FrequencyCounter} class provides a client for
+ * reading in a sequence of words and printing a word (exceeding
+ * a given length) that occurs most frequently. It is useful as
+ * a test client for various symbol table implementations.
+ * <p>
+ * For additional documentation, see <a href="https://algs4.cs.princeton.edu/31elementary">Section 3.1</a> of
+ * <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
  *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
  */
 public class FrequencyCounter {
 
     // Do not instantiate.
-    private FrequencyCounter() { }
+    private FrequencyCounter() {
+    }
 
     /**
      * Reads in a command-line integer and sequence of words from
@@ -54,6 +55,7 @@ public class FrequencyCounter {
      */
     public static void main(String[] args) {
         int distinct = 0, words = 0;
+        String lastPut = "";
         int minlen = Integer.parseInt(args[0]);
         ST<String, Integer> st = new ST<String, Integer>();
 
@@ -62,10 +64,38 @@ public class FrequencyCounter {
             String key = StdIn.readString();
             if (key.length() < minlen) continue;
             words++;
+            lastPut = key;
             if (st.contains(key)) {
                 st.put(key, st.get(key) + 1);
+            } else {
+                st.put(key, 1);
+                distinct++;
             }
-            else {
+        }
+
+        // find a key with the highest frequency count
+        String max = "";
+        st.put(max, 0);
+        for (String word : st.keys()) {
+            if (st.get(word) > st.get(max))
+                max = word;
+        }
+
+        StdOut.println(max + " " + st.get(max));
+        StdOut.println("lastPut = " + lastPut);
+        StdOut.println("distinct = " + distinct);
+        StdOut.println("words    = " + words);
+    }
+
+    public static void count(String[] a) {
+        int distinct = 0, words = 0;
+        ST<String, Integer> st = new ST<String, Integer>();
+
+        for (String key : a) {
+            words++;
+            if (st.contains(key)) {
+                st.put(key, st.get(key) + 1);
+            } else {
                 st.put(key, 1);
                 distinct++;
             }
