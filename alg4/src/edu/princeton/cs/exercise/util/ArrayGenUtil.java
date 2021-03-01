22793fbdaeec6c2cf9061959bf1b8f9506775e3e
Leon Chen <chenlinfeng1024@gmail.com>
2021-03-01 10:18:11 +0800

    add util

---
+++ alg4/src/edu/princeton/cs/exercise/util/ArrayGenUtil.java
@@ -0,0 +1,78 @@
+package edu.princeton.cs.exercise.util;
+
+import edu.princeton.cs.algs4.StdRandom;
+
+/**
+ * 数组生成工具
+ *
+ * @author LeonChen
+ * @since 2021/2/25
+ */
+public class ArrayGenUtil {
+
+    public static Comparable[] genRandomArr2Values(int length) {
+        Comparable[] array = new Comparable[length];
+
+        for (int i = 0; i < length; i++) {
+            array[i] = StdRandom.uniform(2);
+        }
+
+        return array;
+    }
+
+    public static Comparable[] genArrAllSameKey(int length) {
+        Comparable[] array = new Comparable[length];
+
+        for (int i = 0; i < length; i++) {
+            array[i] = 0;
+        }
+
+        return array;
+    }
+
+    public static Comparable[] genReverseOrderedArr(int length) {
+        Comparable[] array = new Comparable[length];
+
+        for (int i = 0; i < length; i++) {
+            array[i] = length - 1 - i;
+        }
+
+        return array;
+    }
+
+    public static Comparable[] genOrderedArr(int length) {
+        Comparable[] array = new Comparable[length];
+
+        for (int i = 0; i < length; i++) {
+            array[i] = i;
+        }
+
+        return array;
+    }
+
+    public static String[] genRandomStrArr(int length, int minStringLength, int maxStringLength) {
+        String[] array = new String[length];
+
+        for (int i = 0; i < length; i++) {
+            array[i] = genRandomStr(minStringLength, maxStringLength);
+        }
+
+        return array;
+    }
+
+    public static String genRandomStr(int minStringLength, int maxStringLength) {
+
+        StringBuilder randomString = new StringBuilder();
+
+        int stringSize = StdRandom.uniform(minStringLength, maxStringLength + 1);
+
+        for (int i = 0; i < stringSize; i++) {
+            char randomChar = (char) StdRandom.uniform(Constants.UPPER_LETTER_INITIAL_INDEX,
+                    Constants.LOWER_LETTER_FINAL_INDEX + 1);
+            randomString.append(randomChar);
+        }
+
+        return randomString.toString();
+    }
+
+}
---
+++ alg4/src/edu/princeton/cs/exercise/util/Constants.java
@@ -0,0 +1,12 @@
+package edu.princeton.cs.exercise.util;
+
+/**
+ * @author LeonChen
+ * @since 2021/2/25
+ */
+public class Constants {
+    public static final int LOWER_LETTER_INITIAL_INDEX = 97;
+    public static final int LOWER_LETTER_FINAL_INDEX = 122;
+    public static final int UPPER_LETTER_INITIAL_INDEX = 65;
+    public static final int UPPER_LETTER_FINAL_INDEX = 90;
+}
