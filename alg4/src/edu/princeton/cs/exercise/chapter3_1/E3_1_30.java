0da5ae9560c8c2da8998f8d1e4139f99fe36b633
Leon Chen <chenlinfeng1024@gmail.com>
2021-03-11 20:54:53 +0800

    update 3.1.29&30

---
+++ alg4/src/edu/princeton/cs/exercise/chapter3_1/E3_1_30.java
@@ -0,0 +1,31 @@
+package edu.princeton.cs.exercise.chapter3_1;
+
+import edu.princeton.cs.algs4.BinarySearchST;
+
+/**
+ * 3.1.30 Certification. Add assert statements to BinarySearchST to check algorithm
+ * invariants and data structure integrity after every insertion and deletion. For example,
+ * every index i should always be equal to rank(select(i)) and the array should always
+ * be in order.
+ * <p>
+ * 认证.添加 assert 语句到 BinarySearchST 来检查每次插入和删除后算法的不变量和数据结构完整性.比如,每个 index
+ * i 应该总是等于 rank(select(i)) 并且数组总是有序的
+ *
+ * @author LeonChen
+ * @since 2/25/20
+ */
+class E3_1_30 {
+
+
+    /**
+     * 官方以实现
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        BinarySearchST binarySearchST = new BinarySearchST();
+
+    }
+
+
+}
