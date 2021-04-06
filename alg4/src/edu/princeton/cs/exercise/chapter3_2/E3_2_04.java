9681ef062f427cfdeb240ad6e9571b69a4fcf21f
Leon Chen <chenlinfeng1024@gmail.com>
2021-04-06 23:02:46 +0800

    update 3.2.4&5

---
+++ alg4/src/edu/princeton/cs/exercise/chapter3_2/E3_2_04.java
@@ -0,0 +1,35 @@
+package edu.princeton.cs.exercise.chapter3_2;
+
+/**
+ * 3.2.4 Suppose that a certain BST has keys that are integers between 1 and 10, and we
+ * search for 5. Which sequence below cannot be the sequence of keys examined?
+ * a.10,9,8,7,6,5
+ * b.4,10,8,7,53
+ * c.1,10,2,9,3,8,4,7,6,5
+ * d.2,7,3,8,4,5
+ * e.1,2,10,4,8,5
+ * <p>
+ * 假设一个 BST 拥有键是整数在 1 到 10 之间,并且我们查找 5.下面哪个序列不可能是检查的键?
+ * a.10,9,8,7,6,5
+ * b.4,10,8,7,53
+ * c.1,10,2,9,3,8,4,7,6,5
+ * d.2,7,3,8,4,5
+ * e.1,2,10,4,8,5
+ *
+ * @author LeonChen
+ * @since 4/1/20
+ */
+class E3_2_04 {
+
+    /**
+     * @formatter:off
+     * d 是错误的.
+     * 根据左右子树的规律可以知道,如果你抄左子树查找,那么接下来查找的节点一定小于根节点.反之则大于.
+     * d 8 这个节点是不可能出现在 7 的左子树的
+     */
+    public static void main(String[] args) {
+    }
+
+
+
+}
---
+++ alg4/src/edu/princeton/cs/exercise/chapter3_2/E3_2_05.java
@@ -0,0 +1,30 @@
+package edu.princeton.cs.exercise.chapter3_2;
+
+/**
+ * 3.2.5 Suppose that we have an estimate ahead of time of how often search keys are to
+ * be accessed in a BST, and the freedom to insert them in any order that we desire.
+ * Should the keys be inserted into the tree in increasing order, decreasing order of
+ * likely frequency of access, or some other order? Explain your answer.
+ * <p>
+ * 假设我们有一个对BST 中搜索键访问的频率的时间估算,并且可以按照我们想要的顺序自由的插入他们.可以将键按照升序
+ * 插入到树中,或者是访问频率的降序插入,或者是其他的顺序?解释你的答案
+ *
+ * @author LeonChen
+ * @since 4/2/20
+ */
+class E3_2_05 {
+
+    /**
+     * todo
+     * @formatter:off
+     * 可以使用
+     * 1. Knuth's dynamic programming algorithm
+     * 2- Mehlhorn's approximation algorithm
+     *
+     * 可以参考 https://en.wikipedia.org/wiki/Optimal_binary_search_tree
+     */
+    public static void main(String[] args) {
+    }
+
+
+}
