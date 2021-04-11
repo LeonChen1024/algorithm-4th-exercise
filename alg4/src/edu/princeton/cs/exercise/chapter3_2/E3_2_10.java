a940d922ed87b735ccc69ae62f8746cb48afb989
Leon Chen <chenlinfeng1024@gmail.com>
2021-04-11 09:57:45 +0800

    update 3.2.10&11&12

---
+++ alg4/src/edu/princeton/cs/exercise/chapter3_2/E3_2_10.java
@@ -0,0 +1,124 @@
+package edu.princeton.cs.exercise.chapter3_2;
+
+import edu.princeton.cs.algs4.BST;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * 3.2.10 Write a test client TestBST.java for use in testing the implementations of
+ * min(), max(), floor(), ceiling(), select(), rank(), delete(), deleteMin(),
+ * deleteMax(), and keys() that are given in the text. Start with the standard indexing
+ * client given on page 370. Add code to take additional command-line arguments, as
+ * appropriate.
+ * <p>
+ * 编写一个测试客户端 TestBST.java 用来测试正文中 min(), max(), floor(), ceiling(), select(),
+ * rank(), delete(), deleteMin(), deleteMax(), 和 keys() 实现.从370 页标准索引客户端开始.
+ * 添加代码来接受合适的额外命令行参数
+ *
+ * @author LeonChen
+ * @since 4/7/20
+ */
+class E3_2_10 {
+
+    /**
+     * @formatter:off
+     *
+     * 官方实现如下
+     * @formatter:on
+     */
+    public static void main(String[] args) {
+    }
+
+    /******************************************************************************
+     *  Compilation:  javac TestBST.java
+     *  Execution:    java TestBST
+     *  Dependencies: BST.java
+     *
+     *  A test client for BST.java.
+     *
+     *  % java -ea TestBST
+     *
+     ******************************************************************************/
+
+    public static class TestBST {
+        public static void main(String[] args) {
+            String test = "S E A R C H E X A M P L E";
+            String[] keys = test.split(" ");
+            int n = keys.length;
+            BST<String, Integer> st = new BST<String, Integer>();
+            for (int i = 0; i < n; i++)
+                st.put(keys[i], i);
+
+            StdOut.println("size = " + st.size());
+            StdOut.println("min  = " + st.min());
+            StdOut.println("max  = " + st.max());
+            StdOut.println();
+
+
+            // print keys in order using allKeys()
+            StdOut.println("Testing keys()");
+            StdOut.println("--------------------------------");
+            for (String s : st.keys())
+                StdOut.println(s + " " + st.get(s));
+            StdOut.println();
+
+            // print keys in order using select
+            StdOut.println("Testing select");
+            StdOut.println("--------------------------------");
+            for (int i = 0; i < st.size(); i++)
+                StdOut.println(i + " " + st.select(i));
+            StdOut.println();
+
+            // test rank, floor, ceiling
+            StdOut.println("key rank floor flor2 ceil");
+            StdOut.println("-------------------------");
+            for (char i = 'A'; i <= 'X'; i++) {
+                String s = i + "";
+                StdOut.printf("%2s %4d %4s %4s %4s\n", s, st.rank(s), st.floor(s), st.floor2(s), st.ceiling(s));
+            }
+            StdOut.println();
+
+            // test range search and range count
+            String[] from = {"A", "Z", "X", "0", "B", "C"};
+            String[] to = {"Z", "A", "X", "Z", "G", "L"};
+            StdOut.println("range search");
+            StdOut.println("-------------------");
+            for (int i = 0; i < from.length; i++) {
+                StdOut.printf("%s-%s (%2d) : ", from[i], to[i], st.size(from[i], to[i]));
+                for (String s : st.keys(from[i], to[i]))
+                    StdOut.print(s + " ");
+                StdOut.println();
+            }
+            StdOut.println();
+
+            // delete the smallest keys
+            for (int i = 0; i < st.size() / 2; i++) {
+                st.deleteMin();
+            }
+            StdOut.println("After deleting the smallest " + st.size() / 2 + " keys");
+            StdOut.println("--------------------------------");
+            for (String s : st.keys())
+                StdOut.println(s + " " + st.get(s));
+            StdOut.println();
+
+            // delete all the remaining keys
+            while (!st.isEmpty()) {
+                st.delete(st.select(st.size() / 2));
+            }
+            StdOut.println("After deleting the remaining keys");
+            StdOut.println("--------------------------------");
+            for (String s : st.keys())
+                StdOut.println(s + " " + st.get(s));
+            StdOut.println();
+
+            StdOut.println("After adding back the keys");
+            StdOut.println("--------------------------------");
+            for (int i = 0; i < n; i++)
+                st.put(keys[i], i);
+            for (String s : st.keys())
+                StdOut.println(s + " " + st.get(s));
+            StdOut.println();
+
+        }
+    }
+
+}
---
+++ alg4/src/edu/princeton/cs/exercise/chapter3_2/E3_2_11.java
@@ -0,0 +1,27 @@
+package edu.princeton.cs.exercise.chapter3_2;
+
+/**
+ * 3.2.11 How many binary tree shapes of N nodes are there with height N? How many
+ * different ways are there to insert N distinct keys into an initially empty BST that result
+ * in a tree of height N? (See Exercise 3.2.2.)
+ * <p>
+ * 高度为 N 节点数量为 N 的二叉树有集中形状?有多少种不同的方式插入 N 中不同的键到空 BST 中会出现高度为 N
+ * 的树?
+ *
+ * @author LeonChen
+ * @since 4/8/20
+ */
+class E3_2_11 {
+
+    /**
+     * @formatter:off
+     * 除了根节点,其他的节点都只有左右子树两个节点可选,所以是 $ 2^{N-1} $种形状.
+     * 由于每层只有一个节点.插入顺序一定和层高一致,所以一旦交换一定会导致树的形状发生变化.所以只有
+     * $ 2^{N-1} $种输入方式
+     * @formatter:on
+     */
+    public static void main(String[] args) {
+    }
+
+
+}
