76ee1ccff9501550a8b8acf61211439617f86420
Leon Chen <chenlinfeng1024@gmail.com>
2021-06-23 22:43:42 +0800

    update 3.3.8&9

---
+++ alg4/src/edu/princeton/cs/exercise/chapter3_3/E3_3_08.java
@@ -0,0 +1,41 @@
+package edu.princeton.cs.exercise.chapter3_3;
+
+/**
+ * 3.3.8 Show all possible ways that one might represent a 4-node with three 2-nodes
+ * bound together with red links (not necessarily left-leaning).
+ * <p>
+ * 列出所有 3 个 2-节点通过红色连接(不一定是要左斜)可以代表一个 4-节点的所有情况
+ *
+ * @author LeonChen
+ * @since 6/15/21
+ */
+class E3_3_08 {
+
+    /**
+     * @formatter:off
+     *
+     *   A
+     *     B
+     *      C
+     *
+     *    A
+     *     C
+     *    B
+     *
+     *    B
+     *   A C
+     *
+     *    C
+     *   A
+     *    B
+     *
+     *     C
+     *    B
+     *   A
+     * @formatter:on
+     */
+    public static void main(String[] args) {
+    }
+
+
+}
---
+++ alg4/src/edu/princeton/cs/exercise/chapter3_3/E3_3_09.java
@@ -0,0 +1,45 @@
+package edu.princeton.cs.exercise.chapter3_3;
+
+/**
+ * 3.3.9 Which of the following are red-black BSTs?
+ * @formatter:off
+ * i
+ *          c
+ *      rA         Y
+ *              H
+ *           rD
+ *
+ * ii
+ *                   E
+ *             rD           H
+ *          C       F   G       Z
+ *      rA
+ *
+ * iii
+ *                      E
+ *                B             Y
+ *            A     D       H       Z
+ *
+ * iiii
+ *                      H
+ *                 C            Y
+ *             rA           rT
+ *
+ * @formatter:on
+ *
+ * 下列哪些是红黑树?
+ * @author LeonChen
+ * @since 6/16/21
+ */
+class E3_3_09 {
+
+    /**
+     * @formatter:off
+     *  iii  iiii 是
+     * @formatter:on
+     */
+    public static void main(String[] args) {
+    }
+
+
+}
