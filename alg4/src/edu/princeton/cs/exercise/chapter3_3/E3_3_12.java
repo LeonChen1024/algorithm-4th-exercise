305d1b68eda408cbcc05083ad9501297d3b066db
Leon Chen <chenlinfeng1024@gmail.com>
2021-06-27 13:55:14 +0800

    update 3.3.12

---
+++ alg4/src/edu/princeton/cs/exercise/chapter3_3/E3_3_12.java
@@ -0,0 +1,64 @@
+package edu.princeton.cs.exercise.chapter3_3;
+
+/**
+ * 3.3.12 Draw the red-black BST that results after each transformation (color flip or
+ * rotation) during the insertion of P for our standard indexing client.
+ *
+ * @author LeonChen
+ * @since 6/17/21
+ */
+class E3_3_12 {
+
+    /**
+     * @formatter:off
+     *                              
+     *                             R
+     *                      rE                X
+     *                  C        M       rS
+     *              rA       rH
+     * -------------------------------------------------
+     * 插入 P
+     *                             R
+     *                      rE                X
+     *                  C        M       rS
+     *              rA       rH    rP
+     * -------------------------------------------------
+     * 翻转
+     *                             R
+     *                      rE                X
+     *                  C       rM       rS
+     *              rA        H     P
+     * -------------------------------------------------
+     * 左旋
+     *                               R
+     *                      rM                X
+     *                 rE        P       rS
+     *               C     H
+     *            rA
+     * -------------------------------------------------
+     * 右旋
+     *                          M
+     *                  rE           rR
+     *                C     H      P       X
+     *            rA                    rS
+     * -------------------------------------------------
+     * 翻转
+     *                         rM
+     *                   E            R
+     *                C     H      P       X
+     *            rA                    rS
+     * -------------------------------------------------
+     * 根节点置黑
+     *                          M
+     *                   E            R
+     *                C     H      P       X
+     *            rA                  rS
+     * -------------------------------------------------
+     *
+     * @formatter:on
+     */
+    public static void main(String[] args) {
+    }
+
+
+}
