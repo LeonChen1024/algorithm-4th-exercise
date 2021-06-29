b1555e6e90f06ebb28337ede34c7d8cd1f045ccd
Leon Chen <chenlinfeng1024@gmail.com>
2021-06-29 22:36:02 +0800

    update 3.3.13&14

---
+++ alg4/src/edu/princeton/cs/exercise/chapter3_3/E3_3_13.java
@@ -0,0 +1,24 @@
+package edu.princeton.cs.exercise.chapter3_3;
+
+/**
+ * 3.3.13 True or false: If you insert keys in increasing order into a red-black BST,
+ * the tree height is monotonically increasing.
+ * <p>
+ * 对还是错:如果你往一个红黑树中按递增顺序插入元素,树的高度是单调递增的
+ *
+ * @author LeonChen
+ * @since 6/18/21
+ */
+class E3_3_13 {
+
+    /**
+     * @formatter:off
+     * 对.
+     *
+     * @formatter:on
+     */
+    public static void main(String[] args) {
+    }
+
+
+}
