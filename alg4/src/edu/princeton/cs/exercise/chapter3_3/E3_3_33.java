89b5f1bc24bac4e738c5dd439a9a5e3f45252163
Leon Chen <chenlinfeng1024@gmail.com>
2021-07-14 22:02:58 +0800

    update 3.3.33&34

---
+++ alg4/src/edu/princeton/cs/exercise/chapter3_3/E3_3_33.java
@@ -0,0 +1,30 @@
+package edu.princeton.cs.exercise.chapter3_3;
+
+/**
+ * 3.3.33 Certification. Add to RedBlackBST a method is23() to check that no node is
+ * connected to two red links and that there are no right-leaning red links and a method
+ * isBalanced() to check that all paths from the root to a null link have the same number
+ * of black links. Combine these methods with code from isBST() in Exercise 3.2.32 to
+ * create a method isRedBlackBST() that checks that the tree is a red-black BST.
+ * <p>
+ * 验证.添加一个 is23()方法到 RedBlackBST 来检查没有节点是有两个红色链接并且没有右侧红色链接并添加一个
+ * isBalanced() 方法来检查所有从根节点到 null 链接都拥有相同的黑色链接.结合这些方法和 练习 3.2.32 的
+ * isBST() 来创建一个 isRedBlackBST() 方法检查一个树是不是一个红黑树
+ *
+ * @author LeonChen
+ * @since 7/5/21
+ */
+class E3_3_33 {
+
+    /**
+     * @formatter:off
+     * RedBlackBST 已经实现
+     * 都是按照递归思路处理即可
+     * @formatter:on
+     */
+    public static void main(String[] args) {
+
+    }
+
+
+}
