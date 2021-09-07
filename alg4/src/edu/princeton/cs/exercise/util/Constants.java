2fc36e6de78241ae24846b51468d4ae976b9aca7
Leon Chen <chenlinfeng1024@gmail.com>
2021-09-07 10:06:54 +0800

    edit

--- alg4/src/edu/princeton/cs/exercise/util/Constants.java
+++ alg4/src/edu/princeton/cs/exercise/util/Constants.java
@@ -9,4 +9,5 @@ public class Constants {
     public static final int LOWER_LETTER_FINAL_INDEX = 122;
     public static final int UPPER_LETTER_INITIAL_INDEX = 65;
     public static final int UPPER_LETTER_FINAL_INDEX = 90;
+    public static final String FILES_PATH = "src/edu/princeton/cs/algs4-data/";
 }
